7be02bbc68830c263ae6fea0cb5ce183f08e6ed7
Elijah <emwangi372@gmail.com>
2023-03-21 18:48:20 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -6,26 +6,26 @@
  * Return: Always 0.
  */
 
-(int main(void)
+int main(void)
 {
 	int r;
 
-	r = _print_sign(98);
+	r = print_sign(98);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	r = _print_sign(0);
+	r = print_sign(0);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	r = _print_sign(0xff);
+	r = print_sign(0xff);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	r = _print_sign(-1);
+	r = print_sign(-1);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
